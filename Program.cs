efba281cebd57d14827535c11244dc64e31d5eb5
Y-Daantje <302867649@student.rocmondriaan.nl>
2026-01-15 18:56:12 +0100

    ..

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Immutable;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System;
+
+namespace FileSystemTree
+{
+    class FileProgram
+    {
+        static void Main(string[] args)
+        {
+            string baseDirectoryPath = GetBaseDirectoryPath();
+            DirectoryInfo baseDirectory = new DirectoryInfo(baseDirectoryPath);
+
+            FileSystemTreeItem fileSystemTree = GetFileSystemTree(baseDirectory);
+
+            OutputFileSystemTreeLevel(fileSystemTree, 0);
+        }
+
+        static void OutputFileSystemTreeLevel(int indentationLevel, FileSystemTreeItem item)
+        {
+            
+        }
+        static FileSystemTreeItem GetFileSystemTree()
+        {
+            
+        }
+        static string GetBaseDirectoryPath()
+        {
+         
+        }
+    }
+}
